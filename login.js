50d8ef90c72850a495a98afe07a8bce3f7e66970
Tyler <tylerclancy030@gmail.com>
2022-03-16 16:00:23 -0500

    Get handle on UI elements

---
+++ login.js
@@ -0,0 +1,17 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDXSK_DpqIerCRyx6Lo4FvHZEh81Gr2vSM",
+  authDomain: "second-project-fc8fd.firebaseapp.com",
+  databaseURL: "https://second-project-fc8fd-default-rtdb.firebaseio.com",
+  projectId: "second-project-fc8fd",
+  storageBucket: "second-project-fc8fd.appspot.com",
+  messagingSenderId: "198455979766",
+  appId: "1:198455979766:web:e5ad1252ab1327a7ae06d7"
+};
+
+// Get elements
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const login = document.getElementById('login');
+const signup = document.getElementById('signup');
+const logout = document.getElementById('logout');
