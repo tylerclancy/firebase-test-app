4d87f16df881fdc08af842c268f3aa8d6e957d73
Tyler <tylerclancy030@gmail.com>
2022-03-16 15:25:41 -0500

    Initialize firebase

---
+++ storage.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDXSK_DpqIerCRyx6Lo4FvHZEh81Gr2vSM",
+  authDomain: "second-project-fc8fd.firebaseapp.com",
+  databaseURL: "https://second-project-fc8fd-default-rtdb.firebaseio.com",
+  projectId: "second-project-fc8fd",
+  storageBucket: "second-project-fc8fd.appspot.com",
+  messagingSenderId: "198455979766",
+  appId: "1:198455979766:web:e5ad1252ab1327a7ae06d7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
